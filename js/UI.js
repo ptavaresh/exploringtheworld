79ed698a8a71f6f6a215f5a43bb598704156dbcb
Pedro Tavares <ptavaresh@gmail.com>
2019-12-01 22:29:23 -0600

    getting current location and click to get position

--- js/UI.js
+++ js/UI.js
@@ -8,18 +8,31 @@ class UI {
          this.markers = new L.LayerGroup();
          // Iniciar el mapa
          this.mapa = this.inicializarMapa();
-
+         //
     }
 
     inicializarMapa() {
          // Inicializar y obtener la propiedad del mapa
-         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
+         const map = L.map('mapa').setView([19.390519, -99.3739778], 16);
+
+         //centra el mapa en tu zona actual
+         map.locate({setView: true, maxZoom: 10});
+
          const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
          L.tileLayer(
              'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
              attribution: '&copy; ' + enlaceMapa + ' Contributors',
              maxZoom: 18,
              }).addTo(map);
+             //obtener lat y long al clickear el mapa
+             map.on('click', 
+             function(e){
+                 var coord = e.latlng.toString().split(',');
+                 var lat = coord[0].split('(');
+                 var lng = coord[1].split(')');
+                 console.log("You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]);
+             });
+             
          return map;
 
     }
@@ -59,6 +72,27 @@ class UI {
 
             this.markers.addLayer(marker);
         });
+
+        //crear icono para ubicacion actual
+        var greenIcon = L.icon({
+            iconUrl: 'leaf-green.png',
+            shadowUrl: 'leaf-shadow.png',
+        
+            iconSize:     [38, 95], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+        //obtener la localizacion
+        var {lat, lng} = this.mapa._lastCenter
+        const whereiam = new L.marker([
+            parseFloat(lat),
+            parseFloat(lng)],
+            {icon: greenIcon}
+            ).bindPopup("Estas por aqui...").openPopup();
+        this.markers.addLayer(whereiam);
+        // agregar todo al mapa
         this.markers.addTo(this.mapa)
     }
 
