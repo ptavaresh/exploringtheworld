bc6954da929e123277f90fa06cee1580bacb4110
Pedro Tavares <ptavaresh@gmail.com>
2019-12-02 20:57:07 -0600

    added https in the API url

--- js/API.js
+++ js/API.js
@@ -1,7 +1,7 @@
 class API {
     async obtenerDatos() {
         //datos desde el api
-        const datos = await fetch(`http://exploringtheworld.pythonanywhere.com/places/`);
+        const datos = await fetch(`https://exploringtheworld.pythonanywhere.com/places/`);
         //const datos = await fetch(`http://127.0.0.1:8000/places/`);
         //retornar json
         const respuestaJSON = await datos.json();
